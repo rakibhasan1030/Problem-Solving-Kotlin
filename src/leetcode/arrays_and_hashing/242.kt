aa11a5cfb68c2969046eb3dc8061a85e08eabb96
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-05 18:19:23 +0600

    initiate 242

---
+++ src/leetcode/arrays_and_hashing/242.kt
@@ -0,0 +1,17 @@
+package leetcode.arrays_and_hashing
+
+/**
+ * Problem No. 242
+ *
+ * Problem Title : Valid Anagram
+ *
+ * Category : Array and Hashing
+ *
+ * @see [Leetcode_242](https://leetcode.com/problems/valid-anagram/description/)
+ */
+class Solution242 {
+    fun isAnagram(s: String, t: String): Boolean {
+
+        return true
+    }
+}
