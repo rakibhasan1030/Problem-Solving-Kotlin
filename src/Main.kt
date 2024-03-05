91de25a0be34c726820dbaa2e9642201dc5e70ce
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-05 15:08:14 +0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+import leetcode.arrays_and_hashing.Solution217
+
+fun main() {
+    println("Hello World!")
+    Solution217().containsDuplicate(intArrayOf(1,2,1,3,4))
+}
---
+++ src/leetcode/arrays_and_hashing/217.kt
@@ -0,0 +1,8 @@
+package leetcode.arrays_and_hashing
+
+// 217. Contains Duplicate (https://leetcode.com/problems/contains-duplicate/description/)
+class Solution217 constructor() {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        return false
+    }
+}
