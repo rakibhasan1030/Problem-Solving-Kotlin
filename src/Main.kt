321be1f57d6c3197794c06c2a343e15a8305d081
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-05 16:24:36 +0600

    Solve 217, with some optimal solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,5 @@
 import leetcode.arrays_and_hashing.Solution217
 
 fun main() {
-    println("Hello World!")
-    Solution217().containsDuplicate(intArrayOf(1,2,1,3,4))
+    println("Result = ${Solution217().containsDuplicateOptimalSingleLine(intArrayOf(1, 2, 3))}")
 }
--- src/leetcode/arrays_and_hashing/217.kt
+++ src/leetcode/arrays_and_hashing/217.kt
@@ -1,8 +1,66 @@
 package leetcode.arrays_and_hashing
 
-// 217. Contains Duplicate (https://leetcode.com/problems/contains-duplicate/description/)
-class Solution217 constructor() {
-    fun containsDuplicate(nums: IntArray): Boolean {
+/**
+ * Problem No. 217
+ *
+ * Problem Title : Contains Duplicate
+ *
+ * Category : Array and Hashing
+ *
+ * @see [Leetcode_217](https://leetcode.com/problems/contains-duplicate/description/)
+ */
+class Solution217 {
+    fun containsDuplicateBruteForce(nums: IntArray): Boolean {
+        for (i in nums.indices){
+            for (j in i+1..<nums.size){
+                if (nums[i] == nums[j]) return true
+            }
+        }
         return false
     }
+
+    fun containsDuplicateOptimal(nums: IntArray): Boolean {
+        nums.sort()
+        for (i in 0..<nums.size-1){
+            if (nums[i] == nums[i+1]) return true
+        }
+        return false
+    }
+
+    fun containsDuplicateOptimalSingleLine(nums: IntArray): Boolean = nums.size > nums.toSet().size
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
