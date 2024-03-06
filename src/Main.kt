941dddc42904991285e09ca0326b85982f05d536
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-06 15:48:20 +0600

    242 solved with brute force

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,7 @@
 import leetcode.arrays_and_hashing.Solution217
+import leetcode.arrays_and_hashing.Solution242
 
 fun main() {
-    println("Result = ${Solution217().containsDuplicateOptimalSingleLine(intArrayOf(1, 2, 3))}")
+    //println("Result = ${Solution217().containsDuplicateOptimalSingleLine(intArrayOf(1, 2, 3))}")
+    println("Result = ${Solution242().isAnagram("aacc", "ccac")}")
 }
--- src/leetcode/arrays_and_hashing/242.kt
+++ src/leetcode/arrays_and_hashing/242.kt
@@ -10,8 +10,33 @@ package leetcode.arrays_and_hashing
  * @see [Leetcode_242](https://leetcode.com/problems/valid-anagram/description/)
  */
 class Solution242 {
-    fun isAnagram(s: String, t: String): Boolean {
-
-        return true
+    fun isAnagramBruteForce(s: String, t: String): Boolean {
+        val map = mutableMapOf<String, Int>()
+        for(i in s){
+            println(i)
+            val key = i.toString()
+            if (map.containsKey(key)){
+                map[key] = (map[key]?.plus(1))!!
+            } else {
+                map[key] = 1
+            }
+        }
+        for(i in t){
+            println(i)
+            val key = i.toString()
+            if (map.containsKey(key)){
+                if(map[key]!! == 0) return false
+                map[key] = (map[key]?.minus(1))!!
+            } else {
+                return false
+            }
+        }
+        return (map.values.sum()) == 0
     }
+
+    fun isAnagramOptimal(s: String, t: String): Boolean {
+
+        return false
+    }
+
 }
