01520e65fd2065b621c134fb5b4e9aba75e27b67
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-10 18:11:36 +0600

    working on 392

---
+++ src/leetcode/arrays_and_hashing/392.kt
@@ -0,0 +1,17 @@
+package leetcode.arrays_and_hashing
+
+/**
+ * Problem No. 392
+ *
+ * Problem Title : Is Subsequence
+ *
+ * Category : Array and Hashing
+ *
+ * @see [Leetcode_392](https://leetcode.com/problems/is-subsequence/description/)
+ */
+class Solution392 {
+    fun isSubsequenceBruteForce(s: String, t: String): Boolean {
+
+        return true
+    }
+}
