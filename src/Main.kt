d3108b804d4a77cd25d4241cb0a380bae605d516
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-11 12:56:31 +0600

    working on 58

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 import leetcode.arrays_and_hashing.*
 
 fun main() {
-    println("Result = ${Solution392().isSubsequenceOptimal(s = "abc", t = "aaabbbccc")}")
+    println("Result = ${Solution58().lengthOfLastWord(s = "Hello World")}")
 }
---
+++ src/leetcode/arrays_and_hashing/58.kt
@@ -0,0 +1,17 @@
+package leetcode.arrays_and_hashing
+
+
+/**
+ * Problem No. 58
+ *
+ * Problem Title : Length of Last Word
+ *
+ * Category : Array and Hashing
+ *
+ * @see [Leetcode_58](https://leetcode.com/problems/length-of-last-word/description/)
+ */
+class Solution58 {
+    fun lengthOfLastWord(s: String): Int {
+        return 0
+    }
+}
