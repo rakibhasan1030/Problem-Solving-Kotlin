216556fd5585956967a68efed0a334688e087f08
Rakib <hasan.rakib@datasoft-bd.com>
2024-03-14 15:19:33 +0600

    working on 14

---
+++ src/leetcode/arrays_and_hashing/14.kt
@@ -0,0 +1,17 @@
+package leetcode.arrays_and_hashing
+
+/**
+ * Problem No. 14
+ *
+ * Problem Title : Longest Common Prefix
+ *
+ * Category : Array and Hashing
+ *
+ * @see [Leetcode_58](https://leetcode.com/problems/longest-common-prefix/description/)
+ *
+ */
+class Solution14 {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        return ""
+    }
+}
